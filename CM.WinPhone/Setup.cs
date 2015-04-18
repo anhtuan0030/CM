7859c15d70fd439572aa65508c686507bfe6d292
ndlongphi <nguyendolongphi@hotmail.com>
2015-04-18 11:01:43 +0700

    them project  win phone 8.0

---
+++ CM.WinPhone/Setup.cs
@@ -0,0 +1,24 @@
+using Cirrious.CrossCore.Platform;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.WindowsPhone.Platform;
+using Microsoft.Phone.Controls;
+
+namespace CM.WinPhone
+{
+    public class Setup : MvxPhoneSetup
+    {
+        public Setup(PhoneApplicationFrame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new CM.Core.App();
+        }
+		
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+    }
+}
