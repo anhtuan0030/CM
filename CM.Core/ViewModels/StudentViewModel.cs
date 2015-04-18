7d00d0cc33f2bd38ef01a9722e7d5387e714292a
anhtuan0030 <anhtuan0030@gmail.com>
2015-04-18 10:14:16 +0700

    Add view model

---
+++ CM.Core/ViewModels/StudentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.Core.ViewModels
+{
+    public class StudentViewModel : MvxViewModel
+    {
+        private string _hello = "Hello MvvmCross";
+        public string Hello
+        {
+            get { return _hello; }
+            set { _hello = value; RaisePropertyChanged(() => Hello); }
+        }
+    }
+}
