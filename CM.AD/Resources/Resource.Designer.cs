203ab19a538223590f25a4b88c2293ff0af18d43
anhtuan0030 <anhtuan0030@gmail.com>
2015-04-22 10:49:59 +0700

    Commit AD Teacher

--- CM.AD/Resources/Resource.Designer.cs
+++ CM.AD/Resources/Resource.Designer.cs
@@ -90,8 +90,11 @@ namespace CM.AD
 			// aapt resource value: 0x7f040000
 			public const int MvxBindingTagUnique = 2130968576;
 			
+			// aapt resource value: 0x7f040002
+			public const int MyButton = 2130968578;
+			
 			// aapt resource value: 0x7f040001
-			public const int MyButton = 2130968577;
+			public const int listViewTeacher = 2130968577;
 			
 			static Id()
 			{
@@ -110,10 +113,16 @@ namespace CM.AD
 			public const int FirstView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int item_Teacher = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int SplashScreen = 2130903042;
+			public const int ListTeacherView = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int Main = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int SplashScreen = 2130903044;
 			
 			static Layout()
 			{
